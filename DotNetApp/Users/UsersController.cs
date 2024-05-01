9be11afb863703c16100fde2908bb73f86d52727
Thorn Chorn <chornthorn.kh@gmail.com>
2024-05-01 23:16:47 +0700

    fix: add api docs

--- DotNetApp/Users/UsersController.cs
+++ DotNetApp/Users/UsersController.cs
@@ -2,39 +2,45 @@ using DotNetApp.Core;
 using DotNetApp.Core.attribute;
 using DotNetApp.Users.Dto;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace DotNetApp.Users;
 
 [Public]
 [ApiController]
 [Route("api/users")]
-public class UsersController(UsersService usersService): Controller
+public class UsersController(UsersService usersService) : Controller
 {
     [HttpGet]
+    [SwaggerOperation(Summary = "Get all users", Description = "Get all users", OperationId = "GetUsers")]
     public async Task<ActionResult<Response<List<UserDto>>>> GetUsers()
     {
         return await usersService.GetUsers();
     }
-    
+
     [HttpGet("{id}")]
+    [SwaggerOperation(Summary = "Get user by id", Description = "Get user by id", OperationId = "GetUser")]
     public async Task<ActionResult<Response<UserDto>>> GetUser(string id)
     {
         return await usersService.GetUserById(id);
     }
-    
+
     [HttpPost]
+    [SwaggerOperation(Summary = "Create user", Description = "Create user", OperationId = "CreateUser")]
     public async Task<ActionResult<Response<UserDto>>> CreateUser([FromBody] CreateUserDto userDto)
     {
         return await usersService.CreateUser(userDto);
     }
-    
+
     [HttpPut("{id}")]
-    public async Task<ActionResult<Response<UserDto>>> UpdateUser(string id,[FromBody] UpdateUserDto userDto)
+    [SwaggerOperation(Summary = "Update user", Description = "Update user", OperationId = "UpdateUser")]
+    public async Task<ActionResult<Response<UserDto>>> UpdateUser(string id, [FromBody] UpdateUserDto userDto)
     {
-       return await usersService.UpdateUser(id, userDto);
+        return await usersService.UpdateUser(id, userDto);
     }
-    
+
     [HttpDelete("{id}")]
+    [SwaggerOperation(Summary = "Delete user", Description = "Delete user", OperationId = "DeleteUser")]
     public async Task<ActionResult<Response<UserDto>>> DeleteUser([FromRoute] string id)
     {
         return await usersService.DeleteUser(id);
