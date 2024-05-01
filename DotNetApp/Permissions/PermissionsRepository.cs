377c063d59efc1607aeed4b0d3f39ef53098aa3e
Thorn Chorn <chornthorn.kh@gmail.com>
2024-05-01 22:57:05 +0700

    feat: add logs of lifetime of dependency

--- DotNetApp/Permissions/PermissionsRepository.cs
+++ DotNetApp/Permissions/PermissionsRepository.cs
@@ -1,9 +1,22 @@
 using DotNetApp.Core.attribute;
+using DotNetApp.Core.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNetApp.Permissions;
 
 [Repository]
-public class PermissionsRepository
+public class PermissionsRepository(AppDbContext dbContext)
 {
-    
+    public async Task<List<string>> GetPermissionsAsync()
+    {
+       var permissions = new List<string>
+       {
+           "View Users",
+           "Add Users",
+           "Edit Users",
+           "Delete Users"
+       };
+       
+       return permissions;
+    }
 }
